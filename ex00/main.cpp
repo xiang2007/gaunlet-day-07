9045cae3e4d04dc747fb6c2a88231f0043dfa6bb
Wee Shou Xiang <63289858+xiang2007@users.noreply.github.com>
2026-08-15 09:13:40 +0000

    red: added check for has function

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,6 +7,6 @@ TEST_CASE("adding an item raises the count") {
     Inventory inv;
     inv.add("potion");
     CHECK(inv.count() == 1);
-   // CHECK(inv.has("potion") == 1);
-   // CHECK(inv.has("potion") == 0);
+   CHECK(inv.has("potion") == 1);
+   CHECK(inv.has("potion") == 0);
 }
